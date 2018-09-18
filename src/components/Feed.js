2351719a0e70256ebf8674a7f61f41c52907fcdc
react7967 <react7967@caelumBSB01-12>
2018-09-18 20:14:18 -0300

    ScrollView no Feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,16 +1,26 @@
 import React, {Component} from 'react';
-import {Text, View, Dimensions, Image} from 'react-native';
+import {Text, View, Dimensions, Image, ScrollView} from 'react-native';
 
 const width = Dimensions.get('screen').width;
 
 export default class Feed extends Component {
     render(){
+        const fotos = [
+            {id: 1, usuaraio: 'rafael'},
+            {id: 2, usuaraio: 'alberto'},
+            {id: 3, usuaraio: 'vitor'},
+        ];
+
         return(
-            <View>
-                <Text>Rafael</Text>
-                <Image source={require('../../resources/img/alura.png')}
-                style={{width: width, height: width}} />  
-            </View>
+            <ScrollView>
+                {fotos.map(foto => 
+                    <View key={foto.id}>
+                        <Text>{foto.usuaraio}</Text>
+                        <Image source={require('../../resources/img/alura.png')}
+                        style={{width: width, height: width}} />
+                    </View>
+                )}
+            </ScrollView>
         );
     }
 }
