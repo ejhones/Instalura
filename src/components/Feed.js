a376dd22ffabec3b13e2536e6ae4f30e689cf105
react7967 <react7967@caelumBSB01-12>
2018-09-18 20:21:06 -0300

    FlatList no Feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View, Dimensions, Image, ScrollView} from 'react-native';
+import {Text, View, Dimensions, Image, FlatList} from 'react-native';
 
 const width = Dimensions.get('screen').width;
 
@@ -12,15 +12,16 @@ export default class Feed extends Component {
         ];
 
         return(
-            <ScrollView>
-                {fotos.map(foto => 
-                    <View key={foto.id}>
-                        <Text>{foto.usuaraio}</Text>
-                        <Image source={require('../../resources/img/alura.png')}
-                        style={{width: width, height: width}} />
-                    </View>
-                )}
-            </ScrollView>
+            <FlatList style={{marginTop: 20}}
+                keyExtractor={item => item.id}
+                data={fotos}
+                renderItem={({item}) => 
+                <View>
+                    <Text>{item.usuaraio}</Text>
+                    <Image source={require('../../resources/img/alura.png')}
+                    style={{width: width, height: width}} />
+                </View>
+            }/>
         );
     }
 }
